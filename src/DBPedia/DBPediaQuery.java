f31c9afdf69ca0bbdb6e75eeea08779827aa3635
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2015-06-23 14:46:15 +0100

    Formatting

--- src/DBPedia/DBPediaQuery.java
+++ src/DBPedia/DBPediaQuery.java
@@ -18,151 +18,144 @@ import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
 
 public class DBPediaQuery {
-	
-	public LinkedList<WordMeaningOutputElement> queryDBPedia(String word,int start, int end)
-	{
+
+	public LinkedList<WordMeaningOutputElement> queryDBPedia(String word,
+			int start, int end) {
 		LinkedList<WordMeaningOutputElement> elements = new LinkedList<WordMeaningOutputElement>();
 		word = WordUtils.capitalize(word);
-		
-		  ParameterizedSparqlString qs = new ParameterizedSparqlString( "" +
-	        		"prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"+
-	        			"prefix dbpedia-owl: <http://dbpedia.org/ontology/>\n"+
-	        			"prefix dbpprop: <http://dbpedia.org/property/>\n"+
-	        			"prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
 
-	        			"SELECT DISTINCT * \n"+
-	        			"WHERE { \n"+
-	        		  " ?resource  rdfs:label ?label ;\n"+
-	        			        " dbpedia-owl:abstract ?abstract .\n"+
-	        			   "FILTER ( lang(?abstract) = 'en' ) \n"+ 
-	        			"}");
+		ParameterizedSparqlString qs = new ParameterizedSparqlString(""
+				+ "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
+				+ "prefix dbpedia-owl: <http://dbpedia.org/ontology/>\n"
+				+ "prefix dbpprop: <http://dbpedia.org/property/>\n"
+				+ "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+				+
 
-		  Literal wordLiteral = ResourceFactory.createLangLiteral( word, "en" );
-	        qs.setParam( "label", wordLiteral );
-	        QueryExecution exec = QueryExecutionFactory.sparqlService( "http://dbpedia.org/sparql", qs.asQuery() );
+				"SELECT DISTINCT * \n" + "WHERE { \n"
+				+ " ?resource  rdfs:label ?label ;\n"
+				+ " dbpedia-owl:abstract ?abstract .\n"
+				+ "FILTER ( lang(?abstract) = 'en' ) \n" + "}");
 
-	        // Normally you'd just do results = exec.execSelect(), but I want to 
-	        // use this ResultSet twice, so I'm making a copy of it.  
-	        ResultSet results = ResultSetFactory.copyResults( exec.execSelect() );
+		Literal wordLiteral = ResourceFactory.createLangLiteral(word, "en");
+		qs.setParam("label", wordLiteral);
+		QueryExecution exec = QueryExecutionFactory.sparqlService(
+				"http://dbpedia.org/sparql", qs.asQuery());
 
-	        while ( results.hasNext() ) {
-	            // name here. Use *just* the name of the variable.
-	        	QuerySolution sol = (QuerySolution) results.next();
-	        	WordMeaningOutputElement o = new WordMeaningOutputElement();
-	        	o.Description = sol.get("?abstract").toString();
-				o.appearingWord = word;
-				o.Source = "DBPedia";
-				o.id = sol.get( "resource" ).toString();
-				o.startAt = start;
-				o.endAt = end;
-				o.URL = sol.get( "resource" ).toString();
-				boolean exists = false;
-				for(int k = 0;k<elements.size();k++)
-				{
-					if(elements.get(k).id.equals(o.id))
-					{
-						exists = true;
-					}
+		// Normally you'd just do results = exec.execSelect(), but I want to
+		// use this ResultSet twice, so I'm making a copy of it.
+		ResultSet results = ResultSetFactory.copyResults(exec.execSelect());
+
+		while (results.hasNext()) {
+			// name here. Use *just* the name of the variable.
+			QuerySolution sol = (QuerySolution) results.next();
+			WordMeaningOutputElement o = new WordMeaningOutputElement();
+			o.Description = sol.get("?abstract").toString();
+			o.appearingWord = word;
+			o.Source = "DBPedia";
+			o.id = sol.get("resource").toString();
+			o.startAt = start;
+			o.endAt = end;
+			o.URL = sol.get("resource").toString();
+			boolean exists = false;
+			for (int k = 0; k < elements.size(); k++) {
+				if (elements.get(k).id.equals(o.id)) {
+					exists = true;
 				}
-				if(!exists){
-					elements.add(o);
+			}
+			if (!exists) {
+				elements.add(o);
+			}
+
+		}
+		word = word.toLowerCase();
+		word = word.substring(0, 1).toUpperCase() + word.substring(1);
+		qs = new ParameterizedSparqlString(""
+				+ "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
+				+ "prefix dbpedia-owl: <http://dbpedia.org/ontology/>\n"
+				+ "prefix dbpprop: <http://dbpedia.org/property/>\n"
+				+ "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+				+
+
+				"SELECT DISTINCT * \n" + "WHERE { \n"
+				+ " ?resource  rdfs:label ?label ;\n"
+				+ " dbpedia-owl:abstract ?abstract .\n"
+				+ "FILTER ( lang(?abstract) = 'en' ) \n" + "}");
+
+		wordLiteral = ResourceFactory.createLangLiteral(word, "en");
+		qs.setParam("label", wordLiteral);
+		exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql",
+				qs.asQuery());
+
+		// Normally you'd just do results = exec.execSelect(), but I want to
+		// use this ResultSet twice, so I'm making a copy of it.
+		results = ResultSetFactory.copyResults(exec.execSelect());
+
+		while (results.hasNext()) {
+			// As RobV pointed out, don't use the `?` in the variable
+			// name here. Use *just* the name of the variable.
+			QuerySolution sol = (QuerySolution) results.next();
+			WordMeaningOutputElement o = new WordMeaningOutputElement();
+			o.Description = sol.get("?abstract").toString();
+			o.appearingWord = word;
+			o.Source = "DBPedia";
+			o.id = sol.get("resource").toString();
+			o.startAt = start;
+			o.endAt = end;
+			o.URL = sol.get("resource").toString();
+			boolean exists = false;
+			for (int k = 0; k < elements.size(); k++) {
+				if (elements.get(k).id.equals(o.id)) {
+					exists = true;
 				}
+			}
+			if (!exists) {
+				elements.add(o);
+			}
 
-	        }
-	        word = word.toLowerCase();
-	        word = word.substring(0,1).toUpperCase()+word.substring(1);
-	         qs = new ParameterizedSparqlString( "" +
-	        		"prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"+
-	        			"prefix dbpedia-owl: <http://dbpedia.org/ontology/>\n"+
-	        			"prefix dbpprop: <http://dbpedia.org/property/>\n"+
-	        			"prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
+		}
 
-	        			"SELECT DISTINCT * \n"+
-	        			"WHERE { \n"+
-	        		  " ?resource  rdfs:label ?label ;\n"+
-	        			        " dbpedia-owl:abstract ?abstract .\n"+
-	        			   "FILTER ( lang(?abstract) = 'en' ) \n"+ 
-	        			"}");
+		// ResultSetFormatter.out( results );
 
-		    wordLiteral = ResourceFactory.createLangLiteral( word, "en" );
-	        qs.setParam( "label", wordLiteral );
-	        exec = QueryExecutionFactory.sparqlService( "http://dbpedia.org/sparql", qs.asQuery() );
-
-	        // Normally you'd just do results = exec.execSelect(), but I want to 
-	        // use this ResultSet twice, so I'm making a copy of it.  
-	        results = ResultSetFactory.copyResults( exec.execSelect() );
-
-	        while ( results.hasNext() ) {
-	            // As RobV pointed out, don't use the `?` in the variable
-	            // name here. Use *just* the name of the variable.
-	        	QuerySolution sol = (QuerySolution) results.next();
-	        	WordMeaningOutputElement o = new WordMeaningOutputElement();
-	        	o.Description = sol.get("?abstract").toString();
-				o.appearingWord = word;
-				o.Source = "DBPedia";
-				o.id = sol.get( "resource" ).toString();
-				o.startAt = start;
-				o.endAt = end;
-				o.URL = sol.get( "resource" ).toString();
-				boolean exists = false;
-				for(int k = 0;k<elements.size();k++)
-				{
-					if(elements.get(k).id.equals(o.id))
-					{
-						exists = true;
-					}
-				}
-				if(!exists){
-					elements.add(o);
-				}
-
-	        }
-	        
-
-	       // ResultSetFormatter.out( results );
-		
-		
 		return elements;
 	}
-	
-	 public static void main(String[] args) {
-	        ParameterizedSparqlString qs = new ParameterizedSparqlString( "" +
-	        		"prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"+
-	        			"prefix dbpedia-owl: <http://dbpedia.org/ontology/>\n"+
-	        			"prefix dbpprop: <http://dbpedia.org/property/>\n"+
-	        			"prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
 
-	        			"SELECT DISTINCT * \n"+
-	        			"WHERE { \n"+
-	        		  " ?resource  rdfs:label ?label ;\n"+
-	        			        " dbpedia-owl:abstract ?abstract .\n"+
-	        			   "FILTER ( lang(?abstract) = 'en' ) \n"+ 
-	        			"}");
+	public static void main(String[] args) {
+		ParameterizedSparqlString qs = new ParameterizedSparqlString(""
+				+ "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
+				+ "prefix dbpedia-owl: <http://dbpedia.org/ontology/>\n"
+				+ "prefix dbpprop: <http://dbpedia.org/property/>\n"
+				+ "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+				+
 
-	        Literal london = ResourceFactory.createLangLiteral( "London", "en" );
-	        qs.setParam( "label", london );
+				"SELECT DISTINCT * \n" + "WHERE { \n"
+				+ " ?resource  rdfs:label ?label ;\n"
+				+ " dbpedia-owl:abstract ?abstract .\n"
+				+ "FILTER ( lang(?abstract) = 'en' ) \n" + "}");
 
-	        System.out.println( qs );
+		Literal london = ResourceFactory.createLangLiteral("London", "en");
+		qs.setParam("label", london);
 
-	        QueryExecution exec = QueryExecutionFactory.sparqlService( "http://dbpedia.org/sparql", qs.asQuery() );
+		System.out.println(qs);
 
-	        // Normally you'd just do results = exec.execSelect(), but I want to 
-	        // use this ResultSet twice, so I'm making a copy of it.  
-	        ResultSet results = ResultSetFactory.copyResults( exec.execSelect() );
+		QueryExecution exec = QueryExecutionFactory.sparqlService(
+				"http://dbpedia.org/sparql", qs.asQuery());
 
-	        while ( results.hasNext() ) {
-	            // As RobV pointed out, don't use the `?` in the variable
-	            // name here. Use *just* the name of the variable.
-	        	QuerySolution sol = (QuerySolution) results.next();
-	        	RDFNode node = sol.get( "resource" );
-	        	
-	        	System.out.println(sol.get("?abstract"));
-	        	
-	        
-	            System.out.println( node);
-	        }
+		// Normally you'd just do results = exec.execSelect(), but I want to
+		// use this ResultSet twice, so I'm making a copy of it.
+		ResultSet results = ResultSetFactory.copyResults(exec.execSelect());
 
-	        // A simpler way of printing the results.
-	        ResultSetFormatter.out( results );
-	    }
+		while (results.hasNext()) {
+			// As RobV pointed out, don't use the `?` in the variable
+			// name here. Use *just* the name of the variable.
+			QuerySolution sol = (QuerySolution) results.next();
+			RDFNode node = sol.get("resource");
+
+			System.out.println(sol.get("?abstract"));
+
+			System.out.println(node);
+		}
+
+		// A simpler way of printing the results.
+		ResultSetFormatter.out(results);
+	}
 }
