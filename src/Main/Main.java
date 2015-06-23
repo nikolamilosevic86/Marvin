300a9b32d324c96399459bede376de5af12254bf
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2015-06-23 16:41:53 +0100

    Formatting

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -17,7 +17,7 @@ import WordNet.Wordnet;
 public class Main {
 	public static String text;
 	public static Tokenizer tokenizer;
-	public static LinkedList<Word> words = new LinkedList<Word> ();
+	public static LinkedList<Word> words = new LinkedList<Word>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -27,54 +27,50 @@ public class Main {
 		DBPediaQuery db = new DBPediaQuery();
 		InputStream is;
 		try {
-			
-			Tokenizer _tokenizer = null;
-			 
 			InputStream modelIn = null;
-			   // Loading tokenizer model
-			   modelIn = new FileInputStream("en-pos-maxent.bin");
-			   final POSModel posModel = new POSModel(modelIn);
-			   modelIn.close();
-			 
-			   POSTaggerME _posTagger = new POSTaggerME(posModel);
-			 
-			
-			
+			// Loading tokenizer model
+			modelIn = new FileInputStream("en-pos-maxent.bin");
+			final POSModel posModel = new POSModel(modelIn);
+			modelIn.close();
+
+			POSTaggerME _posTagger = new POSTaggerME(posModel);
+
 			is = new FileInputStream("en-token.bin");
-			TokenizerModel model = new TokenizerModel(is);	 
+			TokenizerModel model = new TokenizerModel(is);
 			tokenizer = new TokenizerME(model);
 			String[] tokens = tokenizer.tokenize(text);
 			Span[] tokens2 = tokenizer.tokenizePos(text);
 			String[] tags = _posTagger.tag(tokens);
-			for(int i = 0;i<tokens.length;i++)
-			{
+			for (int i = 0; i < tokens.length; i++) {
 				Word w = new Word();
 				w.starting = tokens2[i].getStart();
 				w.ending = tokens2[i].getEnd();
 				w.word = tokens[i];
-				w.wordmeanings.addAll(wn.getSencesFromWordnet(w.word,tags[i], w.starting, w.ending));
-				w.wordmeanings.addAll(db.queryDBPedia(w.word, w.starting, w.ending));
-				if(i+1<tokens.length){
-				w.wordmeanings.addAll(db.queryDBPedia(w.word + " "+tokens[i+1], w.starting, tokens2[i+1].getEnd()));
+				w.wordmeanings.addAll(wn.getSencesFromWordnet(w.word, tags[i],
+						w.starting, w.ending));
+				w.wordmeanings.addAll(db.queryDBPedia(w.word, w.starting,
+						w.ending));
+				if (i + 1 < tokens.length) {
+					w.wordmeanings.addAll(db.queryDBPedia(w.word + " "
+							+ tokens[i + 1], w.starting,
+							tokens2[i + 1].getEnd()));
 				}
 				words.add(w);
 			}
-			
-			for(int i = 0;i<words.size();i++)
-			{
-				System.out.println("Word: "+words.get(i).word);
+
+			for (int i = 0; i < words.size(); i++) {
+				System.out.println("Word: " + words.get(i).word);
 				System.out.println("Meaninigs:");
-				for(int j = 0;j<words.get(i).wordmeanings.size();j++){
-					System.out.println("Meaninig ("+words.get(i).wordmeanings.get(j).Source+"): "+words.get(i).wordmeanings.get(j).Description);
+				for (int j = 0; j < words.get(i).wordmeanings.size(); j++) {
+					System.out.println("Meaninig ("
+							+ words.get(i).wordmeanings.get(j).Source + "): "
+							+ words.get(i).wordmeanings.get(j).Description);
 				}
 			}
-			
-			
-		}catch(Exception ex)
-		{
+
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
 
 	}
 
--- src/WordNet/Wordnet.java
+++ src/WordNet/Wordnet.java
@@ -12,57 +12,58 @@ import net.didion.jwnl.dictionary.Dictionary;
 
 public class Wordnet {
 	private Dictionary dict = null;
-	//http://wordnetweb.princeton.edu/perl/webwn?s=world
-	
-	public Wordnet()
-	{
+
+	// http://wordnetweb.princeton.edu/perl/webwn?s=world
+
+	public Wordnet() {
 		String propsFile = "file_properties.xml";
 		try {
 			JWNL.initialize(new FileInputStream(propsFile));
 			dict = Dictionary.getInstance();
-		}catch(Exception ex)
-		{
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
-	public LinkedList<WordMeaningOutputElement> getSencesFromWordnet(String word,String tag, int start, int end)
-	{
-		
+
+	public LinkedList<WordMeaningOutputElement> getSencesFromWordnet(
+			String word, String tag, int start, int end) {
+
 		LinkedList<WordMeaningOutputElement> sences = new LinkedList<WordMeaningOutputElement>();
-		try{
-		IndexWord Iword = null;
-		if(tag.equals("NN")||tag.equals("NNP")||tag.equals("NNS")||tag.equals("NNPS"))
-			Iword = dict.lookupIndexWord(POS.NOUN,word);
-		else if(tag.equals("VB")||tag.equals("MD")||tag.equals("VBZ")||tag.equals("VBD")||tag.equals("VBG")||tag.equals("VBN")||tag.equals("VBP")||tag.equals("VBZ"))
-			Iword = dict.lookupIndexWord(POS.VERB,word);
-		else if(tag.equals("JJ")||tag.equals("JJR")||tag.equals("JJS"))
-			Iword = dict.lookupIndexWord(POS.ADJECTIVE,word);
-		else if(tag.equals("RB")||tag.equals("RBR")||tag.equals("RBS"))
-			Iword = dict.lookupIndexWord(POS.ADVERB,word);
-		if(Iword==null)
-			return sences;
-		Synset[] senses = Iword.getSenses();
-		for (int i=0; i<senses.length; i++) {
-			WordMeaningOutputElement o = new WordMeaningOutputElement();
-			System.out.println(senses[i]);
-			o.Description = senses[i].getGloss();
-			o.appearingWord = word;
-			o.Source = "WordNet";
-			o.id = senses[i].getKey()+"";
-			o.startAt = start;
-			o.endAt = end;
-			o.URL = "http://wordnetweb.princeton.edu/perl/webwn?s="+senses[i].getWord(0).getLemma();
-			sences.add(o);
-			
-		}
-		}catch(Exception ex)
-		{
+		try {
+			IndexWord Iword = null;
+			if (tag.equals("NN") || tag.equals("NNP") || tag.equals("NNS")
+					|| tag.equals("NNPS"))
+				Iword = dict.lookupIndexWord(POS.NOUN, word);
+			else if (tag.equals("VB") || tag.equals("MD") || tag.equals("VBZ")
+					|| tag.equals("VBD") || tag.equals("VBG")
+					|| tag.equals("VBN") || tag.equals("VBP")
+					|| tag.equals("VBZ"))
+				Iword = dict.lookupIndexWord(POS.VERB, word);
+			else if (tag.equals("JJ") || tag.equals("JJR") || tag.equals("JJS"))
+				Iword = dict.lookupIndexWord(POS.ADJECTIVE, word);
+			else if (tag.equals("RB") || tag.equals("RBR") || tag.equals("RBS"))
+				Iword = dict.lookupIndexWord(POS.ADVERB, word);
+			if (Iword == null)
+				return sences;
+			Synset[] senses = Iword.getSenses();
+			for (int i = 0; i < senses.length; i++) {
+				WordMeaningOutputElement o = new WordMeaningOutputElement();
+				System.out.println(senses[i]);
+				o.Description = senses[i].getGloss();
+				o.appearingWord = word;
+				o.Source = "WordNet";
+				o.id = senses[i].getKey() + "";
+				o.startAt = start;
+				o.endAt = end;
+				o.URL = "http://wordnetweb.princeton.edu/perl/webwn?s="
+						+ senses[i].getWord(0).getLemma();
+				sences.add(o);
+
+			}
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return sences;
 	}
-	
-	
 
 }
